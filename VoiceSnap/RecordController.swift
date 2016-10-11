e9892c789d0b879290bfdc42476b6378e3415473
Eric Hodgins <hodgins.e@gmail.com>
2016-10-11 00:43:10 -0400

    feat: Ability to record and play.  Saving to Documents directory still buggy.

---
+++ VoiceSnap/RecordController.swift
@@ -0,0 +1,234 @@
+//
+//  ViewController.swift
+//  VoiceSnap
+//
+//  Created by Eric Hodgins on 2016-10-09.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordController: UIViewController {
+    
+    var newSavedFileName: String?
+    var audioPlayer: AVAudioPlayer!
+    var audioEngine: AVAudioEngine!
+    
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    var audioURL: URL = {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths.first!
+        
+        
+        let url = documentDirectory.appendingPathComponent("audioFileName.m4a")
+        
+        return url
+    }()
+    
+    lazy var recordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("RECORD", for: .normal)
+        button.backgroundColor = UIColor.red
+        return button
+    }()
+    
+    lazy var playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.backgroundColor = UIColor.green
+        return button
+    }()
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("SAVE", for: .normal)
+        button.backgroundColor = UIColor.orange
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AVAudioSession.sharedInstance().requestRecordPermission { (success) in
+            print(success)
+        }
+        
+        recordButton.addTarget(self, action: #selector(RecordController.startRecording), for: .touchUpInside)
+        playButton.addTarget(self, action: #selector(RecordController.playRecording), for: .touchUpInside)
+        saveButton.addTarget(self, action: #selector(RecordController.saveAndRenameRecording), for: .touchUpInside)
+        
+        audioEngine = AVAudioEngine()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        view.addSubview(recordButton)
+        recordButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(playButton)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(saveButton)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playButton.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 10),
+            playButton.heightAnchor.constraint(equalToConstant: 100),
+            playButton.widthAnchor.constraint(equalToConstant: 300),
+            
+            recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            recordButton.topAnchor.constraint(equalTo: playButton.bottomAnchor, constant: 10),
+            recordButton.heightAnchor.constraint(equalToConstant: 100.0),
+            recordButton.widthAnchor.constraint(equalToConstant: 300),
+            
+            saveButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            saveButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            saveButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+            saveButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+}
+
+extension RecordController: AVAudioRecorderDelegate {
+    func startRecording() {
+        
+        if recordButton.titleLabel?.text == "RECORD" {
+        
+            let session = AVAudioSession.sharedInstance()
+            
+            do {
+                try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+                try session.setActive(true)
+                let settings = [
+                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                    AVSampleRateKey: 44100,
+                    AVNumberOfChannelsKey: 2,
+                    AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                ]
+                // 4. create the audio recording, and assign ourselves as the delegate
+                audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.isMeteringEnabled = true
+                audioRecorder.prepareToRecord()
+                audioRecorder.record()
+                recordButton.setTitle("STOP", for: .normal)
+            } catch {
+                print(error)
+            }
+        } else {
+            stopRecording()
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("finished recording")
+    }
+    
+    
+    func stopRecording() {
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
+        recordButton.setTitle("RECORD", for: .normal)
+        
+        createAudioPlayer()
+    }
+
+}
+
+
+extension RecordController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Audio player finished playing.")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("Decode error.")
+    }
+    
+    func playRecording() {
+        if audioPlayer != nil {
+            audioPlayer.stop()
+            audioEngine.stop()
+            audioEngine.reset()
+            audioPlayer.play()
+        } else if saveRecording() {
+            createAudioPlayer()
+            let session = AVAudioSession.sharedInstance()
+            do {
+                try session.setCategory(AVAudioSessionCategoryPlayback, with: .defaultToSpeaker)
+            } catch {
+                print(error)
+            }
+            audioPlayer.play()
+        }
+        
+    }
+    
+    func createAudioPlayer() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+            print(audioURL)
+            audioPlayer.enableRate = true
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+        } catch {
+            print(error)
+        }
+    }
+}
+
+extension RecordController {
+    func saveRecording() -> Bool {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options: [])
+            for p in directoryContents {
+                print(p)
+            }
+            if directoryContents.count > 0 {
+                audioURL = directoryContents.last!
+                return true
+            } else {
+                return false
+            }
+        } catch {
+            return false
+        }
+    }
+    
+    func saveAndRenameRecording() {
+        let saveController = SaveTableViewController()
+        let nav = UINavigationController(rootViewController: saveController)
+        present(nav, animated: true, completion: nil)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VoiceSnap/SaveTableViewController.swift
@@ -0,0 +1,152 @@
+//
+//  SaveTableViewController.swift
+//  VoiceSnap
+//
+//  Created by Eric Hodgins on 2016-10-10.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import UIKit
+
+class SaveTableViewController: UITableViewController {
+    
+    lazy var saveTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "My New Recording"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    
+    init() {
+       super.init(style: .grouped)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SaveTableViewController.cancel))
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(SaveTableViewController.save))
+        
+        navigationItem.rightBarButtonItem = saveButton
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.selectionStyle = .none
+        
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            cell.contentView.backgroundColor = UIColor.black
+        case (1, 0):
+            cell.contentView.addSubview(saveTextField)
+            
+            NSLayoutConstraint.activate([
+                saveTextField.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                saveTextField.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+                saveTextField.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor, constant: 16),
+                saveTextField.rightAnchor.constraint(equalTo: cell.contentView.rightAnchor, constant: 20)
+            ])
+            
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Recording"
+        case 1:
+            return "Enter Recording Name"
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            return 100
+        case (1, 0):
+            return tableView.rowHeight
+        default:
+            return tableView.rowHeight
+        }
+    }
+    
+}
+
+
+extension SaveTableViewController {
+    func save() {
+        let docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        print(docDir)
+        var oldURL = docDir.appendingPathComponent("audioFileName.m4a")
+        //let newURL = docDir.appendingPathComponent("NewItem.m4a")
+        oldURL.setTemporaryResourceValue("NewItem.m4a", forKey: .nameKey)
+        
+        //try! FileManager.default.moveItem(at: oldURL, to: newURL)
+    }
+    
+    func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
